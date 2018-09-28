dc0bbe305bf01df555b8bff3fc42b8e55e448663
basiibas <bas.riemprasert@gmail.com>
2018-09-28 19:00:31 +0200

    TIlbake til layout

--- Gruppeoppgave1/Models/DB.cs
+++ Gruppeoppgave1/Models/DB.cs
@@ -1,60 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using System.ComponentModel.DataAnnotations;
-
-
+using System.Linq;
+using System.Web;
 
 namespace Gruppeoppgave1.Models
 {
-    public class Brukere
-    {
-        [Key]
-        public string Epost { get; set; }
-        public string Fornavn { get; set; }
-        public string Etternavn { get; set; }
-        public string Adresse { get; set; }
-        public string Passord { get; set; }
-        public int Telefon { get; set; }
-        public string Følsesdato { get; set; }
-    }
-
-    public class Kategorier
-    {
-        [Key]
-        public int KategoriId { get; set; }
-        public string KatgoriNavn { get; set; }
-    }
-
-    public class Filmer
-    {
-        public int Id { get; set; }
-        public string Navn { get; set; }
-        public string Beskrivelse { get; set; }
-        public double Pris { get; set; }
-
-        public virtual List<Kategori> KategoriId { get; set; }
-    }
-    public class DB: DbContext
+    public class DB : DbContext
     {
         public DB()
-            : base("name=DB")
+            : base("name:kunde")
         {
             Database.CreateIfNotExists();
-            Database.SetInitializer(new DBInit());
         }
-        public virtual DbSet<Bruker> Bruker { get; set; }
-        public virtual DbSet<Film> Filme { get; set; }
-        public virtual DbSet<Kategori> Kategorie { get; set; }
-
+        public DbSet<Kunde> Kunder { get;}
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+           
         }
     }
-        
-    
 }
