602e20f81851c9f2dd7756ee8a6d32490c643ccc
basiibas <bas.riemprasert@gmail.com>
2018-09-29 03:02:52 +0200

    Fikset login,registrering og Entity

--- Gruppeoppgave1/Controllers/HomeController.cs
+++ Gruppeoppgave1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gruppeoppgave1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,17 +12,107 @@ namespace Gruppeoppgave1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var db = new DBBruker();
+            List<Bruker> alleBrukere = db.alleBrukere();
+            return View(alleBrukere);
+
         }
+
 
         public ActionResult Login()
         {
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+
             return View();
+
         }
+
+        [HttpPost]
+        public ActionResult Login(Bruker innBruker)
+        {
+            if (Bruker_i_DB(innBruker))
+            {
+                Session["LoggetInn"] = true;
+                ViewBag.Innlogget = true;
+                return View();
+            }
+            else
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+                return View();
+            }
+        }
+
+
 
         public ActionResult Registry()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Registry(Bruker innBruker)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+
+            }
+            using (var db = new DBContext())
+            {
+                try
+                {
+                    var nyBrukerr = new Brukere();
+
+                    nyBrukerr.Epost = innBruker.Epost;
+
+                    nyBrukerr.Fornavn = innBruker.Fornavn;
+                    nyBrukerr.Etternavn = innBruker.Etternavn;
+                    
+                    nyBrukerr.Adresse = innBruker.Adresse;
+                    nyBrukerr.Telefon = innBruker.Telefon;
+                    nyBrukerr.Fødselsdato = innBruker.Fødselsdato;
+                    nyBrukerr.Passord = innBruker.Passord;
+                
+                    db.Brukere.Add(nyBrukerr);
+                    db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+                catch (Exception feil)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+        }
+
+        private static bool Bruker_i_DB(Bruker innBruker)
+        {
+            using (var db = new DBContext())
+            {
+                Brukere funnetBruker = db.Brukere.FirstOrDefault
+                    (b => b.Epost == innBruker.Epost && b.Passord == innBruker.Passord);
+                if (funnetBruker == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+        }
+
     }
 }
---
+++ Gruppeoppgave1/Models/Bruker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gruppeoppgave1.Models
+{
+    public class Bruker
+    {
+        [Key]
+        public string Epost { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Adresse { get; set; }
+        public string Passord { get; set; }
+        public string Telefon { get; set; }
+        public string Fødselsdato { get; set; }
+    }
+}
