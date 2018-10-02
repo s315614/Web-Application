2425f7b1d5994cd3963ada5a87ec700ea142b533
s315614 <s315614@stud.hioa.no>
2018-10-02 13:24:22 +0200

    validering til registration

---
+++ Gruppeoppgave1/Scripts/validation.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $("#register-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        }
+        
+    });
+    messages: {
+        email: {
+            required: 'Epost må oppgis.',
+            email: '<em>Ugyldig<em> epost adresse.'
+        }
+    }
+
+});
