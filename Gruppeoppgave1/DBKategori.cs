ede251088845245ad4c6435fa5a6389f3fe7230a
s315614 <s315614@stud.hioa.no>
2018-10-03 15:15:11 +0200

    katagori

---
+++ Gruppeoppgave1/DBKategori.cs
@@ -0,0 +1,41 @@
+﻿using Gruppeoppgave1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gruppeoppgave1
+{
+    public class DBKategori
+    {
+        public List<Katagori> AlleKategorier()
+        {
+            using (var db = new DBContext())
+            {
+                List<Katagori> AlleKategorier = db.Kategorier.Select(k => new Katagori
+                {
+                    KategoriId = k.KategoriId,
+                    KatgoriNavn = k.KatgoriNavn
+
+
+                }).ToList();
+
+                return AlleKategorier;
+            }
+        }
+        public Katagori hentkategori(int KategoriId)
+        {
+            using (var db = new DBContext())
+            {
+                Kategorier enKategori = db.Kategorier.Find(KategoriId);
+                var hentetKategori = new Katagori()
+                {
+                    KatgoriNavn = enKategori.KatgoriNavn
+
+                };
+
+                return hentetKategori;
+            }
+        }
+    }
+}
